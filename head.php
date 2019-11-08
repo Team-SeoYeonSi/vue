1032b512f65a2bd3aef2a06013cac4067c726c9e
오윤석 <dhdbstjr98@khu.ac.kr>
2019-11-09 00:30:10 +0900

    알림 제거

--- head.php
+++ head.php
@@ -4,8 +4,5 @@ include_once("./_head.php");
 <nav>
 	<div class="nav-wrapper gnb">
 		<img src="./img/logo_sm.png">
-		<button class="btn-alarm">
-			<i class="fas fa-bell"></i>
-		</button>
 	</div>
 </nav>

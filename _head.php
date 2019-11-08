9861ce5538fdc24bf35be7f7d3eee42689f85bf7
오윤석 <dhdbstjr98@khu.ac.kr>
2019-11-08 23:32:16 +0900

    레이아웃

--- _head.php
+++ _head.php
@@ -13,6 +13,7 @@ $API_URL = "https://api.seoyeonsi.bu.to";
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+	<link rel="stylesheet" href="./css/style.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
--- layout.php
+++ layout.php
@@ -2,11 +2,46 @@
 include("./_head.php");
 ?>
 
+<style>
+.gnb { text-align:center; line-height:4.8; }
+.gnb .btn-alarm { position:absolute; right:10px; background:none; line-height:4.2; border:0; width:40px; height:56px; color:#fff; }
+
+.gnb-bottom { position:absolute; bottom:0; height:auto; width:100%; text-align:center; border-top:1px solid #ddd; }
+.gnb-bottom ul li { padding:5px !important; color:#9e9e9e; }
+.gnb-bottom ul li.active { color:#ef5350; }
+.gnb-bottom ul li i { display:block; font-size:18px; }
+.gnb-bottom ul li span { display:block; font-size:12px; }
+</style>
+
 <nav>
-	<div class="nav-wrapper">
-		
+	<div class="nav-wrapper gnb pink accent-1">
+		<img src="./img/logo_sm.png">
+		<button class="btn-alarm">
+			<i class="fas fa-bell"></i>
+		</button>
 	</div>
 </nav>
+
+<div class="gnb-bottom white">
+	<ul class="row">
+		<li class="col s3 active">
+			<i class="fas fa-home"></i>
+			<span>퀘스트</span>
+		</li>
+		<li class="col s3">
+			<i class="fas fa-comment-alt"></i>
+			<span>인증</span>
+		</li>
+		<li class="col s3">
+			<i class="fas fa-won-sign"></i>
+			<span>보상</span>
+		</li>
+		<li class="col s3">
+			<i class="fas fa-cog"></i>
+			<span>설정</span>
+		</li>
+	</ul>
+</div>
 
 <?php
 include("./_tail.php");

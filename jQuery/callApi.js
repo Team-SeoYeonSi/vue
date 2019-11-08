ed106e7ffa8e2ceeb29d96f534784f88ecc20501
오윤석 <dhdbstjr98@khu.ac.kr>
2019-11-09 06:01:15 +0900

    a

--- jQuery/callApi.js
+++ jQuery/callApi.js
@@ -1,17 +1,32 @@
 
 function callApi(api, json, callback, method="POST"){
-    $.ajax({
-        type: method,
-        url: api,
-        data: json,
-        xhrFields: { withCredentials: true },
-        success: function (response) {
-            console.log('response' + JSON.stringify(response));
-            callback(response);
-        },
-        error : (err)=>{
-            console.log(err);
-        },
-    });
+	if(method == "POST") {
+		$.ajax({
+			type: method,
+			url: api,
+			data: json,
+			xhrFields: { withCredentials: true },
+			success: function (response) {
+				console.log('response' + JSON.stringify(response));
+				callback(response);
+			},
+			error : (err)=>{
+				console.log(err);
+			},
+		});
+	} else {
+		$.ajax({
+			type: method,
+			url: api,
+			xhrFields: { withCredentials: true },
+			success: function (response) {
+				console.log('response' + JSON.stringify(response));
+				callback(response);
+			},
+			error : (err)=>{
+				console.log(err);
+			},
+		});
+	}
 }
 

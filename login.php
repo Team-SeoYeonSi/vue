7dd83f033c0d27c47a7d2cfdf92b97765ccc4b9e
오윤석 <dhdbstjr98@khu.ac.kr>
2019-11-09 05:08:10 +0900

    굵어져랏

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ include("./_head.php");
 <style>
 .logo { font-size:40px; text-align:center; padding-top:40%; height:auto; }
 .buttons { width:80%; margin:0 auto; margin-top:30px; }
-.buttons button { width:100%; height:50px; border:0; border-radius:10px; margin-top:50px; }
+.buttons button { width:100%; height:50px; border:0; border-radius:10px; margin-top:50px; font-weight:bold; }
 .buttons .btn-kakao { background-color:#ffde00; color:#000; vertical-align:middle; }
 .buttons .btn-kakao img { vertical-align:middle; margin-top:-4px; margin-right:10px; }
 </style>
